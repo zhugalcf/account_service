6b7d71c62fe4786f551b8e199da59ebbf35649d3
Andrey <khalyavinandrey@gmail.com>
2023-08-30 23:29:16 +0500

    создал миграцию

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,13 @@
 -- Write your sql migration here!
+CREATE TABLE account (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    number VARCHAR(20) CHECK (LENGTH(number) >= 12),
+    owner_id BIGINT NOT NULL,
+    owner_type VARCHAR(128) NOT NULL,
+    currency VARCHAR(3) NOT NULL,
+    status VARCHAR(32) NOT NULL,
+    created_at timestamptz DEFAULT current_timestamp,
+    updated_at timestamptz DEFAULT current_timestamp,
+    closed_at timestamptz,
+    version BIGINT NOT NULL DEFAULT 0
+);
