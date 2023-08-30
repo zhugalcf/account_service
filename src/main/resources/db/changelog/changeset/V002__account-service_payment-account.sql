95f3013d62be617823c4973566bf1fcd080ade17
M1khail <mishalion888@gmail.com>
2023-08-30 21:41:29 +0200

    and index for quick search

--- src/main/resources/db/changelog/changeset/V002__account-service_payment-account.sql
+++ src/main/resources/db/changelog/changeset/V002__account-service_payment-account.sql
@@ -2,8 +2,8 @@ CREATE TABLE account
 (
     id           SERIAL PRIMARY KEY,
     number       VARCHAR(20) NOT NULL,
-    owner_id     BIGINT      NOT NULL,
     owner_type   VARCHAR(20) NOT NULL,
+    owner_id     BIGINT      NOT NULL,
     account_type VARCHAR(20) NOT NULL,
     currency     VARCHAR(3)  NOT NULL,
     status       VARCHAR(20) NOT NULL,
