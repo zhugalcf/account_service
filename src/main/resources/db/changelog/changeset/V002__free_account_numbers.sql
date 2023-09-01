41b16f1a2667855db086290f424fc86c516658cd
Matvey <matveynikishaev@gmail.com>
2023-09-01 15:35:20 +0300

    Added account_number table to the database

---
+++ src/main/resources/db/changelog/changeset/V002__free_account_numbers.sql
@@ -0,0 +1,8 @@
+CREATE TABLE free_account_numbers
+(
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    account_type smallint NOT NULL,
+    account_number VARCHAR(20) CHECK (LENGTH(account_number) >= 12) NOT NULL,
+    CONSTRAINT uc_account_type_account_number UNIQUE (account_type, account_number)
+);
+CREATE UNIQUE INDEX account_type_number_idx ON free_account_numbers (account_type, account_number);
