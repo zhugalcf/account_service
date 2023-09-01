ff1b05e897ef2b2fcf01802191965262afb66532
M1khail <mishalion888@gmail.com>
2023-09-01 15:27:48 +0200

    id

--- src/main/java/faang/school/accountservice/model/Account.java
+++ src/main/java/faang/school/accountservice/model/Account.java
@@ -29,7 +29,7 @@ public class Account {
     private OwnerType ownerType;
 
     @Column(name = "owner_id", nullable = false)
-    private Long ownerId;
+    private long ownerId;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "account_type", nullable = false)
