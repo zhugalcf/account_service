0618a80511cbb0677b7ce5e0ef865442f6392b1d
kxnvg <nikita.armeec.88@mail.ru>
2023-09-04 12:21:49 +0300

    Создана сущность Balance

---
+++ src/main/java/faang/school/accountservice/model/Account.java
@@ -0,0 +1,24 @@
+package faang.school.accountservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "number", nullable = false, length = 16)
+    private String number;
+
+    @OneToOne(mappedBy = "account")
+    private Balance balance;
+}
