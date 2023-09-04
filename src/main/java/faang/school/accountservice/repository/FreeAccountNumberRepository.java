850a82ade7acfcac8c6d3ff2c7ca705e18694d49
Matvey <matveynikishaev@gmail.com>
2023-09-04 21:38:43 +0300

    Добавил метод генерации уникального номера аккаунта.

--- src/main/java/faang/school/accountservice/repository/FreeAccountNumberRepository.java
+++ src/main/java/faang/school/accountservice/repository/FreeAccountNumberRepository.java
@@ -1,9 +1,14 @@
 package faang.school.accountservice.repository;
 
+import faang.school.accountservice.model.AccountType;
 import faang.school.accountservice.model.FreeAccountNumber;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FreeAccountNumberRepository extends JpaRepository<FreeAccountNumber, Long> {
+
+    Optional<FreeAccountNumber> findByAccountType(AccountType accountType);
 }
--- src/main/java/faang/school/accountservice/service/FreeAccountNumbersService.java
+++ src/main/java/faang/school/accountservice/service/FreeAccountNumbersService.java
@@ -1,13 +1,40 @@
 package faang.school.accountservice.service;
 
+import faang.school.accountservice.model.AccountType;
+import faang.school.accountservice.model.FreeAccountNumber;
 import faang.school.accountservice.repository.AccountNumberSequenceRepository;
 import faang.school.accountservice.repository.FreeAccountNumberRepository;
+import faang.school.accountservice.util.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FreeAccountNumbersService {
     private final FreeAccountNumberRepository freeAccountNumberRepository;
     private final AccountNumberSequenceRepository accountNumberSequenceRepository;
+
+    @Transactional
+    @Retryable(value = {RuntimeException.class}, maxAttempts = 3, backoff = @Backoff(delay = 1000))
+    public void generateAccountNumber(AccountType accountType) {
+        var sequence = accountNumberSequenceRepository.findByAccountType(accountType).orElseThrow(
+                () -> new NotFoundException("No such account type found")
+        );
+        Long currentCount = accountNumberSequenceRepository.incrementCurrentCount(sequence.getAccountType().ordinal(), sequence.getCurrentCount())
+                .orElseThrow(
+                        () -> {
+                            String errorMessage = "Failed to increment count for:" + accountType;
+                            log.error(errorMessage);
+                            return new RuntimeException(errorMessage);
+                        }
+                );
+        String accountNumber = String.format("%s%08d", accountType.getAssociatedString(), currentCount);
+        FreeAccountNumber freeAccountNumber = FreeAccountNumber.builder().accountType(accountType).accountNumber(accountNumber).build();
+        freeAccountNumberRepository.save(freeAccountNumber);
+    }
 }
