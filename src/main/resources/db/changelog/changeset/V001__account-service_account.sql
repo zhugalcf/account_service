6c3d7fed08ea2ad143d30a30c794ff187cfdff59
kxnvg <nikita.armeec.88@mail.ru>
2023-09-04 11:30:52 +0300

    Создана миграция для создание таблицы balance

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,11 @@
--- Write your sql migration here!
+CREATE TABLE balance (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    account_number character(16) NOT NULL,
+    current_authorization_balance bigint,
+    current_actual_balance bigint,
+    created_at timestamptz DEFAULT current_timestamp NOT NULL,
+    updated_at timestamptz DEFAULT current_timestamp NOT NULL,
+    version bigint DEFAULT 0 NOT NULL,
+
+    FOREIGN KEY (account_number) REFERENCES account(number) ON DELETE CASCADE
+);
