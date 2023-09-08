660e5d37510fee43bdc3e782b2cc68f68a20baa5
OneWayMy <zhenya.ivashchenko.9494@gmail.com>
2023-09-08 13:09:32 -0700

    feature-BC-5960 just commit

---
+++ src/main/java/faang/school/accountservice/controller/SavingsAccountController.java
@@ -0,0 +1,20 @@
+package faang.school.accountservice.controller;
+
+import faang.school.accountservice.dto.SavingsAccountDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/savings")
+@Slf4j
+public class SavingsAccountController {
+
+    private
+
+    public SavingsAccountDto openSavingsAccount() {
+        return
+    }
+}
