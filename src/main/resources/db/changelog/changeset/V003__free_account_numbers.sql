f117992c27deb401eaf4576c33bfa6c25b22a6fa
Dobrovolsky Artem <artdobrov@mail.ru>
2023-09-08 16:47:49 +0300

    BC-5941: free_account_numbers

--- src/main/resources/db/changelog/changeset/V003__free_account_numbers.sql
+++ src/main/resources/db/changelog/changeset/V003__free_account_numbers.sql
@@ -1,6 +1,5 @@
 CREATE TABLE free_account_numbers
 (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
     type VARCHAR(20) NOT NULL,
-    account_number BIGINT NOT NULL
+    account_number VARCHAR(20) NOT NULL
 )
