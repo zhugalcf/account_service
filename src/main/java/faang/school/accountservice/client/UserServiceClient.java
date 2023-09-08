6b34fe42fd6bc6898b2d76abd68c5650c1326f52
Devilserk <demonserker@gmail.com>
2023-09-08 16:10:08 +0600

    added validation

--- src/main/java/faang/school/accountservice/client/UserServiceClient.java
+++ src/main/java/faang/school/accountservice/client/UserServiceClient.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}/api/v1/users")
+@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}/api/v1")
 public interface UserServiceClient {
 
-    @GetMapping("/exists/{id}")
-    Boolean checkUserExist(@PathVariable long id);
+    @GetMapping("/users/exists/{userId}")
+    Boolean checkUserExist(@PathVariable long userId);
 }
--- src/main/java/faang/school/accountservice/validator/ValidOwnerValidator.java
+++ src/main/java/faang/school/accountservice/validator/ValidOwnerValidator.java
@@ -3,6 +3,7 @@ package faang.school.accountservice.validator;
 import faang.school.accountservice.annotations.ValidOwner;
 import faang.school.accountservice.client.ProjectServiceClient;
 import faang.school.accountservice.client.UserServiceClient;
+import faang.school.accountservice.config.context.UserContext;
 import faang.school.accountservice.dto.account.CreateAccountDto;
 import faang.school.accountservice.enums.OwnerType;
 import jakarta.validation.ConstraintValidator;
@@ -13,12 +14,13 @@ import lombok.RequiredArgsConstructor;
 public class ValidOwnerValidator implements ConstraintValidator<ValidOwner, CreateAccountDto> {
     private final UserServiceClient userServiceClient;
     private final ProjectServiceClient projectServiceClient;
+    private final UserContext userContext;
 
     @Override
     public boolean isValid(CreateAccountDto createAccountDto, ConstraintValidatorContext context) {
+        userContext.setUserId(createAccountDto.getOwner().getOwnerId());
         if (createAccountDto.getOwner().getType() == OwnerType.USER) {
-            Boolean a = userServiceClient.checkUserExist(createAccountDto.getOwner().getOwnerId());
-            return a;
+           return userServiceClient.checkUserExist(createAccountDto.getOwner().getOwnerId());
         } else if (createAccountDto.getOwner().getType() == OwnerType.PROJECT) {
             return projectServiceClient.checkProjectExist(createAccountDto.getOwner().getOwnerId());
         }
