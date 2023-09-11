5935f081672fbcd024852d7e7cf33be30f74b470
Dobrovolsky Artem <artdobrov@mail.ru>
2023-09-11 17:36:54 +0300

    BC-5944: AccountNumbersSequenceRepository

--- src/main/java/faang/school/accountservice/model/AccountNumbersSequence.java
+++ src/main/java/faang/school/accountservice/model/AccountNumbersSequence.java
@@ -4,9 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Version;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "account_numbers_sequence")
 public class AccountNumbersSequence {
     @Id
