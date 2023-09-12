2ea24e28baf81e019d6e8d4b698d51c9a03b03db
Viktor Ivanov <avacode78@gmail.com>
2023-09-12 10:27:11 +0700

    implement migration

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,16 @@
--- Write your sql migration here!
+create table account
+(
+    id             bigint primary key,
+    account_number bigint not null unique
+);
+
+create table balance
+(
+    id                    bigint primary key,
+    account_id            bigint  not null unique references account (id),
+    authorization_balance numeric not null,
+    current_balance       numeric not null,
+    created_at            timestamptz default current_timestamp,
+    updated_at            timestamptz default current_timestamp,
+    balance_version       bigint  not null
+);
