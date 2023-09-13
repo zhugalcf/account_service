67cbd5730a6126688ee2fc0c75259654bc9965a2
Dobrovolsky Artem <artdobrov@mail.ru>
2023-09-13 14:55:07 +0300

    BC-5942: FreeAccountNumbersRepository

--- src/main/java/faang/school/accountservice/model/AccountNumber.java
+++ src/main/java/faang/school/accountservice/model/AccountNumber.java
@@ -4,8 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "free_account_numbers")
 public class AccountNumber {
 
