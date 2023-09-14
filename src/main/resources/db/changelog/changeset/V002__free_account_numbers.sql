ba803a551caa947bb8c56fe97fbfd9dbbc37d74e
MalinaInk <nataleshen.ko@yandex.ru>
2023-09-14 19:12:25 +0700

    initial commit

--- src/main/resources/db/changelog/changeset/V002__free_account_numbers.sql
+++ src/main/resources/db/changelog/changeset/V002__free_account_numbers.sql
@@ -6,3 +6,19 @@ CREATE TABLE free_account_numbers
     CONSTRAINT uc_account_type_account_number UNIQUE (account_type, account_number)
 );
 CREATE UNIQUE INDEX account_type_number_idx ON free_account_numbers (account_type, account_number);
+
+CREATE TABLE account_numbers_sequence
+(
+    id            bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    account_type  smallint NOT NULL,
+    current_count BIGINT   NOT NULL DEFAULT 0
+);
+CREATE UNIQUE INDEX account_numbers_sequence_ind ON account_numbers_sequence (account_type, current_count);
+
+INSERT INTO account_numbers_sequence (account_type, current_count)
+VALUES (0, 0), -- INDIVIDUAL
+       (1, 0), -- ENTITY
+       (2, 0); -- FOREIGN_CURRENCY
+
+
+
