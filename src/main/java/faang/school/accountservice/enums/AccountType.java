00b8a45bdd4be88892cb2d0716b96d9b8d3c8467
MalinaInk <nataleshen.ko@yandex.ru>
2023-09-14 18:42:49 +0700

    initial commit

--- src/main/java/faang/school/accountservice/enums/AccountType.java
+++ src/main/java/faang/school/accountservice/enums/AccountType.java
@@ -1,7 +1,15 @@
 package faang.school.accountservice.enums;
 
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum AccountType {
-    INDIVIDUAL,
-    ENTITY,
-    FOREIGN_CURRENCY
-}
+    INDIVIDUAL("5400"),
+    ENTITY("5500"),
+    FOREIGN_CURRENCY("5600");
+
+    private final String identityString;
+    }
