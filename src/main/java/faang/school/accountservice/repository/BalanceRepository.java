c478ea6842b94c58d02673628a6103486afa0e95
queen <youlka25@yandex.ru>
2023-09-17 21:59:31 +0300

    BC-5717 add repository annotation

--- src/main/java/faang/school/accountservice/repository/BalanceRepository.java
+++ src/main/java/faang/school/accountservice/repository/BalanceRepository.java
@@ -2,9 +2,11 @@ package faang.school.accountservice.repository;
 
 import faang.school.accountservice.entity.balance.Balance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface BalanceRepository extends JpaRepository<Balance, Long> {
     Optional<Balance> findByAccountId(Long accountId);
 }
