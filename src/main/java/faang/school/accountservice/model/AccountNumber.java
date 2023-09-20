79b464f6893a76799fbf924800f43bb0f28e0503
Dobrovolsky Artem <artdobrov@mail.ru>
2023-09-20 18:32:49 +0300

    BC-5942

--- src/main/java/faang/school/accountservice/model/AccountNumber.java
+++ src/main/java/faang/school/accountservice/model/AccountNumber.java
@@ -1,7 +1,12 @@
 package faang.school.accountservice.model;
 
+import faang.school.accountservice.enums.AccountType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -18,9 +23,12 @@ import lombok.NoArgsConstructor;
 public class AccountNumber {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     @Column(name = "account_number", length = 20, nullable = false, unique = true)
     private String account_number;
     @Column(name = "type", length = 20, nullable = false)
-    private AccountNumberType type;
+    @Enumerated(EnumType.STRING)
+    private AccountType type;
 
 }
