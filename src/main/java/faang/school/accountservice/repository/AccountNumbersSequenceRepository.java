c075d79057692395e2427a4f333446ed0e8a87bb
Dobrovolsky Artem <artdobrov@mail.ru>
2023-09-20 18:28:30 +0300

    BC-5944: AccountNumbersSequenceRepository

--- src/main/java/faang/school/accountservice/repository/AccountNumbersSequenceRepository.java
+++ src/main/java/faang/school/accountservice/repository/AccountNumbersSequenceRepository.java
@@ -1,9 +1,7 @@
 package faang.school.accountservice.repository;
 
-import faang.school.accountservice.model.AccountNumberType;
+import faang.school.accountservice.enums.AccountType;
 import faang.school.accountservice.model.AccountNumbersSequence;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,19 +9,23 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface AccountNumbersSequenceRepository extends JpaRepository<AccountNumbersSequence, AccountNumberType> {
+public interface AccountNumbersSequenceRepository extends JpaRepository<AccountNumbersSequence, Long> {
 
     @Query(nativeQuery = true,
-            value = "INSERT INTO account_numbers_sequence (type, current) VALUES (:type, 0) " +
-            "WHERE NOT EXISTS (SELECT * FROM account_numbers_sequence WHERE type = :type)")
-    void createNewCounter(@Param("type") AccountNumberType type);
+            value = """
+                    INSERT INTO account_numbers_sequence (type, current, version)
+                    SELECT :type, 0, 0
+                    WHERE NOT EXISTS (SELECT * FROM account_numbers_sequence WHERE type = :type)
+                    """)
+    void createNewCounter(@Param("type") AccountType type);
 
     @Query(nativeQuery = true,
-            value = "IF EXISTS(SELECT * FROM account_numbers_sequence WHERE type = :type) THEN " +
-            "UPDATE account_numbers_sequence SET current = current + 1 WHERE type  = :type " +
-            "RETURN true " +
-            "ELSE " +
-            "RETURN false")
+            value = """
+            UPDATE account_numbers_sequence SET current = current + 1 WHERE type  = :type
+            RETURNING true
+            """)
     @Transactional
-    boolean optimisticIncrement(@Param("type") AccountNumberType type);
+    boolean increment(@Param("type") AccountType type);
+
+    AccountNumbersSequence getAccountNumbersSequenceByType(AccountType type);
 }
