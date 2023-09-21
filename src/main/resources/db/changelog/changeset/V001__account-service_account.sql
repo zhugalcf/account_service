e85a409a3c0dea7fce73e93a26161450a1826485
b.mukhtarkhanov <b.mukhtarkhanov>
2023-09-21 17:12:58 +0600

    create table account

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,13 @@
 -- Write your sql migration here!
+CREATE table account (
+	id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+	number varchar(20) UNIQUE check (LENGTH(number) BETWEEN 12 AND 20) not null,
+	account_owner varchar(128) not null,
+	account_type varchar(64) not null,
+	currency varchar(3) not null,
+	status varchar(64) not null,
+	created_at timestamptz DEFAULT current_timestamp,
+    updated_at timestamptz DEFAULT current_timestamp,
+    closed_at timestamptz DEFAULT current_timestamp,
+    version INTEGER not null
+)
