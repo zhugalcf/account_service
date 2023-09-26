732c7ffb74f0a9ecb0d2cfc415cb6e69c5b18827
kxnvg <nikita.armeec.88@mail.ru>
2023-09-26 12:33:39 +0300

    bugfix эндпоинтов AccountController(добавлены конструкторы без параметров в объкты dto)

--- src/main/java/faang/school/accountservice/dto/Project.java
+++ src/main/java/faang/school/accountservice/dto/Project.java
@@ -2,9 +2,11 @@ package faang.school.accountservice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Project {
     private Long id;
     private String name;
