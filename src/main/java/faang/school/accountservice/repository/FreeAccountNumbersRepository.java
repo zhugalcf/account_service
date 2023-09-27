220c3e5218920caca11ea401a722f53a9968e4d1
Dobrovolsky Artem <artdobrov@mail.ru>
2023-09-27 16:21:40 +0300

    BC-5942

--- src/main/java/faang/school/accountservice/repository/FreeAccountNumbersRepository.java
+++ src/main/java/faang/school/accountservice/repository/FreeAccountNumbersRepository.java
@@ -1,15 +1,15 @@
 package faang.school.accountservice.repository;
 
-import faang.school.accountservice.enums.AccountType;
 import faang.school.accountservice.model.AccountNumber;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface FreeAccountNumbersRepository extends JpaRepository<AccountNumber, Long> {
+
     @Query(nativeQuery = true,
             value = """
                     INSERT INTO free_account_numbers (type, account_number)
@@ -17,16 +17,26 @@ public interface FreeAccountNumbersRepository extends JpaRepository<AccountNumbe
                             WHERE NOT EXISTS 
                             (SELECT * FROM free_account_numbers WHERE type = :type AND account_number = :number)
                     """)
-    void createNewNumber(@Param("type") AccountType type, @Param("number") String number);
+    @Modifying
+    void createNewNumber(@Param("type") String type, @Param("number") String number);
 
     @Query(nativeQuery = true,
             value = """
                     DELETE FROM free_account_numbers 
-                    WHERE id = (SELECT id FROM free_account_numbers WHERE type = :type LIMIT 1)
-                    RETERNING account_number
+                    WHERE id = :id
                     """)
-    @Transactional
-    String getFreeNumber(@Param("type") AccountType type);
+    @Modifying
+    int deleteFreeNumber(@Param("id") long id);
 
-    long countAccountNumberByType(AccountType type);
+    @Query(nativeQuery = true,
+            value = """
+                    SELECT * FROM free_account_numbers WHERE type = :type LIMIT 1
+                                        """)
+    AccountNumber findFreeNumber(@Param("type") String type);
+
+    @Query(nativeQuery = true,
+            value = """
+                    SELECT COUNT(*) FROM free_account_numbers WHERE type = :type
+                    """)
+    long countAccountNumberByType(@Param("type") String type);
 }
