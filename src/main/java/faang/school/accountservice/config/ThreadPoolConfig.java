1dfc7740431b3c420d1e6e307ef53a7b62e94197
Rustam Nigmatullin <rustamnigma@mail.ru>
2023-09-28 18:59:26 +0300

    intermed

--- src/main/java/faang/school/accountservice/config/ThreadPoolConfig.java
+++ src/main/java/faang/school/accountservice/config/ThreadPoolConfig.java
@@ -12,21 +12,21 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class ThreadPoolConfig {
 
-    @Value("${thread-pools:exceed:core}")
+    @Value("${thread-pools.exceed.core}")
     private int exceedCoreSize;
-    @Value("${thread-pools:exceed:max}")
+    @Value("${thread-pools.exceed.max}")
     private int maxExceedCoreSize;
-    @Value("${thread-pools:exceed:alive-time}")
+    @Value("${thread-pools.exceed.alive-time}")
     private int aliveTimeExceed;
-    @Value("${thread-pools:exceed:queue}")
+    @Value("${thread-pools.exceed.queue}")
     private int exceedQueue;
-    @Value("${thread-pools:create:core}")
+    @Value("${thread-pools.create.core}")
     private int createCoreSize;
-    @Value("${thread-pools:create:max}")
+    @Value("${thread-pools.create.max}")
     private int maxCreateCoreSize;
-    @Value("${thread-pools:create:alive-time}")
+    @Value("${thread-pools.create.alive-time}")
     private int aliveTimeCreate;
-    @Value("${thread-pools:create:queue}")
+    @Value("${thread-pools.create.queue}")
     private int createQueue;
 
 
--- src/main/java/faang/school/accountservice/repository/RequestRepository.java
+++ src/main/java/faang/school/accountservice/repository/RequestRepository.java
@@ -1,7 +1,6 @@
 package faang.school.accountservice.repository;
 
 import faang.school.accountservice.model.Request;
-import org.bouncycastle.cert.ocsp.Req;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -18,8 +17,10 @@ public interface RequestRepository extends CrudRepository<Request, Long> {
     @Override
     Request save(Request request) throws DataIntegrityViolationException;
 
-    @Query(nativeQuery = true, value = "SELECT user_id, COUNT(*) as result_count FROM request" +
-            "WHERE (created_at > :time OR updated_at > :time) GROUP BY user_id HAVING COUNT(*) > :maxNumberOfRequests")
+    @Query(nativeQuery = true, value = """
+            SELECT user_id, COUNT(*) as result_count FROM request 
+            WHERE (created_at > :time OR updated_at > :time) GROUP BY user_id HAVING COUNT(*) > :maxNumberOfRequests
+            """)
     Map<Long, Long> findAllGroupedByUserIdForPeriod(ZonedDateTime time, Long maxNumberOfRequests);
 
     @Query(nativeQuery = true, value = "SELECT * FROM request WHERE id = ?1 FOR UPDATE")
