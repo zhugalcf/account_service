7e9d7994ff31199aec85e2c55f3a5e76409c32f1
Dregid <ufl852@gmail.com>
2023-09-29 17:01:42 +0600

    Created entity

--- src/main/resources/db/changelog/changeset/V002__create_request_table.sql
+++ src/main/resources/db/changelog/changeset/V002__create_request_table.sql
@@ -1,16 +1,17 @@
 CREATE TABLE request (
-    uuid varchar PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+    uuid UUID PRIMARY KEY not null,
     user_id bigint not null,
     type INTEGER not null,
     lock bigint not null,
     flag boolean not null,
-    input varchar not null,
+    input json not null,
     status INTEGER not null,
     additionally varchar(256),
     created_at timestamptz DEFAULT current_timestamp,
     updated_at timestamptz DEFAULT current_timestamp,
-    version INTEGER not null,
-    FOREIGN KEY (user_id) REFERENCES User (id)
+    version bigint not null,
+
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE INDEX index_user ON request(user_id);
