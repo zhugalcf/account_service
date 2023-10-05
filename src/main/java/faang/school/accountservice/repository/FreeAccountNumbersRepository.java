c02df9c253a2086e69735d7842ce6b1c2fa0108e
Alex Tarasov <tarasof.av@gmail.com>
2023-10-05 13:02:12 +0300

    fix SQL requests task bc-5719

--- src/main/java/faang/school/accountservice/repository/FreeAccountNumbersRepository.java
+++ src/main/java/faang/school/accountservice/repository/FreeAccountNumbersRepository.java
@@ -15,7 +15,6 @@ public interface FreeAccountNumbersRepository extends JpaRepository<FreeAccountN
 
     long countByAccountType(AccountType accountType);
 
-    @Modifying
     @Query(nativeQuery = true, value = "DELETE FROM free_account_numbers " +
             "WHERE account_type = ( " +
             "SELECT account_type " +
@@ -24,5 +23,6 @@ public interface FreeAccountNumbersRepository extends JpaRepository<FreeAccountN
             "ORDER BY created_at ASC " +
             "LIMIT 1) " +
             "RETURNING account_number")
+    @Modifying
     Optional<String> deleteAndReturnFirstByAccountTypeOrderByCreatedAtAsc(@Param("accountType") int accountType);
 }
