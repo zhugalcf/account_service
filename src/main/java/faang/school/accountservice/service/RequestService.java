253a2d61a99fcc596d9a8051e0631545f5c3782c
Dregid <ufl852@gmail.com>
2023-10-11 16:36:34 +0600

    Fixed lock validation

--- src/main/java/faang/school/accountservice/service/RequestService.java
+++ src/main/java/faang/school/accountservice/service/RequestService.java
@@ -6,7 +6,6 @@ import faang.school.accountservice.dto.request.RequestDto;
 import faang.school.accountservice.dto.request.UpdateRequestDto;
 import faang.school.accountservice.entity.Request;
 import faang.school.accountservice.enums.RequestStatus;
-import faang.school.accountservice.exception.RequestLockIsOccupiedException;
 import faang.school.accountservice.mapper.RequestMapper;
 import faang.school.accountservice.repository.RequestRepository;
 import faang.school.accountservice.validate.RequestValidation;
@@ -58,19 +57,12 @@ public class RequestService {
     @Retryable(maxAttempts = 5, backoff = @Backoff(delay = 5), retryFor = PersistenceException.class)
     public RequestDto openRequest(OpenRequestDto openRequestDto) {
         Request request = requestRepository.findById(openRequestDto.getRequestId())
-                .orElseThrow(EntityNotFoundException::new);
+                .orElseThrow(() -> new EntityNotFoundException(
+                        String.format("Request by id: %s does not exist", openRequestDto.getRequestId())));
+
         if (requestValidation.validateOpeningRequest(openRequestDto, request)) {
             return requestMapper.toDto(request);
         }
-
-        Request withSameLock = requestRepository
-                .findByUserIdAndLockAndOpenIsTrue(openRequestDto.getLock(), request.getUserId())
-                .orElse(null);
-        if (withSameLock != null) {
-            throw new RequestLockIsOccupiedException(String.format(
-                    "The lock is busy with the request: %s", withSameLock.getRequestId()));
-        }
-
         request.setOpen(true);
         request.setStatus(RequestStatus.TO_EXECUTE);
         request.setLock(openRequestDto.getLock());
