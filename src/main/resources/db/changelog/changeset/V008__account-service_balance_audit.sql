24297dfdff8fd5611d70932f845a51f798c46c32
M1khail <mishalion888@gmail.com>
2023-10-11 12:52:40 +0200

    balance audit - fixed v008

--- src/main/resources/db/changelog/changeset/V008__account-service_balance_audit.sql
+++ src/main/resources/db/changelog/changeset/V008__account-service_balance_audit.sql
@@ -1,6 +1,6 @@
 CREATE TABLE balance_audit
 (
-    balance_audit_id bigint PRIMARY KEY,
+    id bigint PRIMARY KEY,
     balance_id bigint NOT NULL,
     balance_audit_version INT NOT NULL,
     authorization_amount NUMERIC(10, 2),
@@ -11,4 +11,4 @@ CREATE TABLE balance_audit
     CONSTRAINT fk_account_id FOREIGN KEY (balance_id) REFERENCES balance (id)
 );
 
-CREATE INDEX idx_balance_id ON BalanceAudit (balance_id);
+CREATE INDEX idx_balance_id ON balance_audit (balance_id);
