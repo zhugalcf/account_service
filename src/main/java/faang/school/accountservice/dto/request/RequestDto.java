9e0352ae8bcafdcaf3bcbbe6906e1e273b2e4288
Dregid <ufl852@gmail.com>
2023-10-16 14:50:10 +0600

    fix files name

--- src/main/java/faang/school/accountservice/dto/request/RequestDto.java
+++ src/main/java/faang/school/accountservice/dto/request/RequestDto.java
@@ -1,20 +1,26 @@
 package faang.school.accountservice.dto.request;
 
 import faang.school.accountservice.enums.RequestType;
+import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class RequestDto {
     private UUID requestId;
     private Long userId;
+    @Null
     private RequestType requestType;
     private String input;
     private String additionally;
+    @Null
     private LocalDateTime createdAt;
+    @Null
     private LocalDateTime updatedAt;
 }
